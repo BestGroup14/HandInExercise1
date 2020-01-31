c6307f9b82dbe4d763f53b65b963b652b2583de3
carolinenissen <201806156@uni.au.dk>
2020-01-31 09:54:34 +0100

    Tilføjelse af Accumulator, nye metoder, Clear og nye tests

--- Calculator.Test.Unit/CalculatorTest.cs
+++ Calculator.Test.Unit/CalculatorTest.cs
@@ -59,6 +59,7 @@ namespace Calculator.Test.Unit
             //Act + Assert
             Assert.That(uut.Divide(15, 5), Is.EqualTo(3));
         }
+
         [Test]
         public void Test_Divider_0()
         {
@@ -68,5 +69,157 @@ namespace Calculator.Test.Unit
             Assert.That(uut.Divide(3, 0), Is.EqualTo(0));
         }
 
+        [Test]
+        public void Test_Accumulator_Add_5()
+        {
+            //Arrange in Setup 
+
+            //Act
+            uut.Add(2, 3);
+
+            //Assert
+            Assert.That(uut.Accumulator, Is.EqualTo(5));
+        }
+
+        [Test]
+        public void Test_Accumulator_Add_Subtract_13()
+        {
+            //Arrange in Setup 
+
+            //Act
+            uut.Add(2, 3);
+            uut.Subtract(20, 7);
+
+            //Assert
+            Assert.That(uut.Accumulator, Is.EqualTo(13));
+        }
+
+        [Test]
+        public void Test_Accumulator_Add_Subtract_0()
+        {
+            //Arrange in Setup 
+
+            //Act
+            uut.Add(2, 3);
+            uut.Subtract(5, 7);
+
+            //Assert
+            Assert.That(uut.Accumulator, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void Test_Accumulator_Add_Clear_zero()
+        {
+            //Arrange in Setup 
+
+            //Act
+            uut.Add(2, 3);
+            uut.Clear();
+
+            //Assert
+            Assert.That(uut.Accumulator, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void Test_Clear_Add1_7()
+        {
+            //Arrange in Setup 
+
+            //Act
+            uut.Clear();
+            uut.Add1(3);
+            uut.Add1(4);
+
+            //Assert
+            Assert.That(uut.Accumulator, Is.EqualTo(7));
+        }
+
+        [Test]
+        public void Test_Clear_Subtract1_12()
+        {
+            //Arrange in Setup 
+
+            //Act
+            uut.Clear();
+            uut.Add1(19);
+            uut.Subtract1(3);
+            uut.Subtract1(4);
+
+            //Assert
+            Assert.That(uut.Accumulator, Is.EqualTo(12));
+        }
+
+        [Test]
+        public void Test_Clear_Multiply1_380()
+        {
+            //Arrange in Setup 
+
+            //Act
+            uut.Clear();
+            uut.Add1(19);
+            uut.Multiply1(10);
+            uut.Multiply1(2);
+
+            //Assert
+            Assert.That(uut.Accumulator, Is.EqualTo(380));
+        }
+
+        [Test]
+        public void Test_Clear_Divide1_5()
+        {
+            //Arrange in Setup 
+
+            //Act
+            uut.Clear();
+            uut.Add1(60);
+            uut.Divide1(6);
+            uut.Divide1(2);
+
+            //Assert
+            Assert.That(uut.Accumulator, Is.EqualTo(5));
+        }
+
+        [Test]
+        public void Test_Clear_Power1_10000()
+        {
+            //Arrange in Setup 
+
+            //Act
+            uut.Clear();
+            uut.Add1(10);
+            uut.Power1(2);
+            uut.Power1(2);
+
+            //Assert
+            Assert.That(uut.Accumulator, Is.EqualTo(10000));
+        }
+
+        [Test]
+        public void Test_Clear_Power1_001()
+        {
+            //Arrange in Setup 
+
+            //Act
+            uut.Clear();
+            uut.Add1(10);
+            uut.Power1(-2);
+
+            //Assert
+            Assert.That(uut.Accumulator, Is.EqualTo(0.01));
+        }
+
+        [Test]
+        public void Test_Clear_Power1_1()
+        {
+            //Arrange in Setup 
+
+            //Act
+            uut.Clear();
+            uut.Add1(10);
+            uut.Power1(0);
+
+            //Assert
+            Assert.That(uut.Accumulator, Is.EqualTo(1));
+        }
     }
 }
--- Calculator/Calc.cs
+++ Calculator/Calc.cs
@@ -8,46 +8,118 @@ namespace Calculator
 {
     public class Calc
     {
+        private double Accu;
+        public double Accumulator
+        {
+            get { return Accu; }
+
+            private set
+            {
+                if (Accu > 0)
+                {
+                    Accumulator = Accu;
+                }
+                else
+                    Accumulator = 0;
+            }
+        }
+
+        public double Add1(double addend)
+        {
+            Accu = Accu + addend;
+
+            return Accu;
+        }
+
+        public double Subtract1(double subtractor)
+        {
+            Accu = Accu - subtractor;
+
+            return Accu;
+        }
+
+        public double Multiply1(double multiplier)
+        {
+            Accu = Accu * multiplier;
+
+            return Accu;
+        }
+
+        public double Power1(double exponent)
+        {
+            if (exponent > 0)
+            {
+                Accu = Math.Pow(Accu, exponent);
+            }
+            else if (exponent < 0)
+            {
+                Accu = (1 / Math.Pow(Accu, -exponent));
+            }
+            else if(exponent == 0)
+            {
+                Accu = 1;
+            }
+
+            return Accu;
+        }
+
+        public double Divide1(double divisor)
+        {
+            if (divisor != 0)
+            {
+                Accu = Accu / divisor;
+                return Accu;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
         public double Add(double a, double b)
         {
-            double result = a + b;
+            Accu = a + b;
 
-            return result;
+            return Accu;
         }
 
         public double Subtract(double a, double b)
         {
-            double result = a - b;
+            Accu = a - b;
 
-            return result;
+            return Accu;
         }
 
         public double Multiply(double a, double b)
         {
-            double result = a * b;
+            Accu = a * b;
 
-            return result;
+            return Accu;
         }
 
         public double Power(double x, double exp)
         {
-            double result = Math.Pow(x,exp);
+            Accu = Math.Pow(x, exp);
 
-            return result;
+            return Accu;
         }
 
         public double Divide(double dividend, double divisor)
         {
             if (divisor != 0)
             {
-                double result = dividend / divisor;
-                return result;
+                Accu = dividend / divisor;
+                return Accu;
             }
             else
             {
                 return 0;
             }
-            
+        }
+
+        public void Clear()
+        {
+            Accu = 0;
         }
 
     }
